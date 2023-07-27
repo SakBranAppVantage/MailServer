b1c9dd2f37f9618057e81e20ec9906df531d7115
Appvantage Bran <saobran.aung@appvantage.asia>
2023-07-27 09:22:49 +0630

    parses and stores mail information into mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
 import express from "express";
 import * as smtpServer from "smtp-server";
-
+import * as mailparser from "mailparser";
+import { MongoClient, Db, Collection } from "mongodb";
 const app = express();
 const port = 3000;
+
+const MONGODB_CONNECTION_STRING =
+  "mongodb+srv://sa:as@cluster0.jnlgbrx.mongodb.net/?retryWrites=true&w=majority";
+const DB_NAME = "Mailhog_clone";
+const COLLECTION_NAME = "emails";
+
+async function storeEmailInMongoDB(
+  db: Db,
+  email: mailparser.ParsedMail,
+  attachments: mailparser.Attachment[]
+): Promise<string> {
+  const collection: Collection = db.collection(COLLECTION_NAME);
+  const result = await collection.insertOne({ email, attachments });
+  return result.insertedId.toString();
+}
 
 const server = new smtpServer.SMTPServer({
   authOptional: true,
@@ -14,8 +30,22 @@ const server = new smtpServer.SMTPServer({
     callback(null, { user: 123 }); // where 123 is the user id or similar property
   },
   onData(stream, session, callback) {
-    console.log(stream);
     stream.pipe(process.stdout); // Print the received email data to the console
+
+    const temp = async (): Promise<mailparser.ParsedMail> =>
+      await mailparser.simpleParser(stream);
+
+    const data = async () => {
+      const email = await temp();
+      console.log(email.to);
+      const client = await MongoClient.connect(MONGODB_CONNECTION_STRING, {});
+      const db = client.db(DB_NAME);
+      const attachments = email.attachments || [];
+      // Store the email in MongoDB
+      const emailId = await storeEmailInMongoDB(db, email, attachments);
+    };
+    data();
+
     stream.on("end", callback);
   },
 });
